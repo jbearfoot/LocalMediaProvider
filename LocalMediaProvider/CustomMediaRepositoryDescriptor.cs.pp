7fd33da784774e5345f56bf932bd1ad1a81a8c03
Johan Björnfot <johan.bjornfot@episerver.com>
2014-08-21 08:47:32 +0200

    First draft of LocalMediaProvider

---
+++ LocalMediaProvider/CustomMediaRepositoryDescriptor.cs.pp
@@ -0,0 +1,63 @@
+﻿using EPiServer;
+using EPiServer.Cms.Shell.UI.UIDescriptors;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell;
+using EPiServer.Shell.ViewComposition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace $rootnamespace$.LocalMediaProvider
+{
+    [ServiceConfiguration(typeof(IContentRepositoryDescriptor))]
+    public class CustomMediaRepositoryDescriptor : MediaRepositoryDescriptor
+    {
+        private IContentProviderManager _providerManager;
+        public CustomMediaRepositoryDescriptor(IContentProviderManager providerManager)
+        {
+            _providerManager = providerManager;
+        }
+
+        public static new string RepositoryKey
+        {
+            get { return "localmedia"; }
+        }
+
+        public override string Key
+        {
+            get
+            {
+                return RepositoryKey;
+            }
+        }
+
+        public override string Name
+        {
+            get { return "Local media"; }
+        }
+
+        public override IEnumerable<ContentReference> Roots
+        {
+            get
+            {
+                return new ContentReference[] { _providerManager.GetProvider(LocalMediaProviderInitialization.ProviderName).EntryPoint };
+            }
+        }
+    }
+
+    [Component]
+    public class CustomMediaMainNavigationComponent : ComponentDefinitionBase
+    {
+        public CustomMediaMainNavigationComponent()
+            : base("epi-cms.component.Media")
+        {
+            Categories = new string[] { "content" };
+            LanguagePath = "/episerver/cms/components/custommedia";
+            SortOrder = 102;
+            PlugInAreas = new string[] { PlugInArea.AssetsDefaultGroup };
+            Settings.Add(new Setting("repositoryKey", CustomMediaRepositoryDescriptor.RepositoryKey));
+        }
+    }
+}
---
+++ LocalMediaProvider/LocalMediaProviderInitialization.cs.pp
@@ -0,0 +1,74 @@
+﻿using EPiServer;
+using EPiServer.Configuration;
+using EPiServer.Core;
+using EPiServer.DataAccess;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Security;
+using EPiServer.Web;
+using EPiServer.Web.Routing;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace $rootnamespace$.LocalMediaProvider
+{
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class LocalMediaProviderInitialization : IInitializableModule
+    {
+        public const string ProviderName = "localfiles";
+
+        public void Initialize(InitializationEngine context)
+        {
+            //Create provider root if it not exist
+            var contentRepository = context.Locate.ContentRepository();
+            var fileRoot = contentRepository.GetBySegment(SiteDefinition.Current.RootPage, ProviderName, LanguageSelector.AutoDetect(true));
+            if (fileRoot == null)
+            {
+                fileRoot = contentRepository.GetDefault<ContentFolder>(SiteDefinition.Current.RootPage);
+                fileRoot.Name = ProviderName;
+                contentRepository.Save(fileRoot, SaveAction.Publish, AccessLevel.NoAccess);
+            }
+
+            //Register provider
+            var contentProviderManager = context.Locate.Advanced.GetInstance<IContentProviderManager>();
+            var configValues = new NameValueCollection();
+            configValues.Add(ContentProviderElement.EntryPointString, fileRoot.ContentLink.ToString());
+            configValues.Add("rootPath", "C:\\");
+            var provider = context.Locate.Advanced.GetInstance<LocalMediaProvider>();
+            provider.Initialize(ProviderName, configValues);
+            contentProviderManager.ProviderMap.AddProvider(provider);
+
+            //Since we have our structure outside asset root we register routes for it
+            RouteTable.Routes.MapContentRoute(
+                name: "LocalMedia",
+                url: "localfiles/{node}/{partial}/{action}",
+                defaults: new { action = "index" },
+                contentRootResolver: (s) => fileRoot.ContentLink);
+
+            RouteTable.Routes.MapContentRoute(
+                name: "LocalMediaEdit",
+                url: CmsHomePath + "localfiles/{language}/{medianodeedit}/{partial}/{action}",
+                defaults: new { action = "index" },
+                contentRootResolver: (s) => fileRoot.ContentLink);
+        }
+
+        private static string CmsHomePath
+        {
+            get
+            {
+                var cmsContentPath = VirtualPathUtility.AppendTrailingSlash(EPiServer.Shell.Paths.ToResource("CMS", "Content"));
+                return VirtualPathUtilityEx.ToAppRelative(cmsContentPath).Substring(1);
+            }
+        }
+
+        public void Preload(string[] parameters)
+        {}
+
+        public void Uninitialize(EPiServer.Framework.Initialization.InitializationEngine context)
+        {}
+    }
+}
